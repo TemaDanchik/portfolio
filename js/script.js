6a729fe0a0f2487a24100fdcbc1c045893816bba
TemaDanchik <34292144+TemaDanchik@users.noreply.github.com>
2017-12-18 10:55:30 +0900

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,25 @@
+$(document).ready(function(){
+    $(".button-collapse").sideNav();
+    $('.collapsible').collapsible();  
+});
+
+
+  $('.collapsible').collapsible('open', 0);
+  $('.collapsible').collapsible('close', 0);
+  $('.collapsible').collapsible('destroy');
+
+
+$(document).ready(function(){
+      $('.slider').slider();
+    });
+
+
+$('.slider').slider('pause');
+$('.slider').slider('start');
+$('.slider').slider('next');
+$('.slider').slider('prev');
+
+
+$(document).ready(function(){
+      $('.parallax').parallax();
+    });
